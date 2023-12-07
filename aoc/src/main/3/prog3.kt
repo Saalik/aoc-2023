cc97644de117b17da6f5ab43c1f8c19166d803ee
saalik <saalik.hatia@outlook.com>
2023-12-07 18:25:22 +0100

    Added Day 3 part 2

--- aoc/src/main/3/prog3.kt
+++ aoc/src/main/3/prog3.kt
@@ -1,10 +1,14 @@
 import java.io.File
 
+data class Gear (val row : Int, val col : Int)
+
+
 fun main() {
-    val inputFilePath = "./src/main/3/small"
+
+    val inputFilePath = "./aoc/src/main/3/small"
     val activationPoint = mutableMapOf<Int, MutableList<Int>>()
-    val gearMap = mutableMapOf<Int, MutableList<Int>>()
-    val gears = mutableMapOf<Gear, MutableList<Int>>()
+
+    val gears = mutableMapOf<Gear, MutableList<Int>>() // Map of gear to list of numbers
 
     val lines = File(inputFilePath).useLines { it.toList() } // Read file lines once
     val height = lines.size
@@ -21,7 +25,6 @@ fun main() {
             }
             if ( char == '*' ) {
                 gears[Gear(rowIndex, colIndex)] = mutableListOf()
-                gearMap.addToMultimap(rowIndex, colIndex)
             }
         }
     }
@@ -32,13 +35,11 @@ fun main() {
     charArray.forEachIndexed { line, chars ->
         numberBuilder.clear()
         var isValid = false
-        val gearList = mutableListOf<Gear>()
 
         chars.forEachIndexed { col, char ->
             if (char.isDigit()) {
                 numberBuilder.append(char)
                 isValid = isValid || searchNeighbourSignal(line, col, activationPoint)
-                searchNeighbourGear(line, col, gearMap).let { gear -> gearList += gear }
             } else {
                 if (numberBuilder.isNotEmpty()) {
                     if (isValid) {
@@ -50,38 +51,28 @@ fun main() {
             }
         }
         if (numberBuilder.isNotEmpty()) {
-            if ( isValid) {
+            if ( isValid ) {
                 res += numberBuilder.toString().toInt()
             }
-            for (gear in gearList) {
-                println("This gear is addded $gear")
-                gears.addToMultimap(gear, numberBuilder.toString().toInt())
-            }
         }
+
     }
     println("Result is $res")
 
-    gears.forEach { (gear, numbers) ->
-        println("Gear before at (row=${gear.row}, col=${gear.col}): $numbers")
+    gears.forEach { (gear, _) ->
+        gears[gear] = searchGearNeighbours(gear, charArray).toMutableList()
     }
 
-//    val filteredGears = gears.filter { (_, numList) ->  numList.size == 2}
-//    filteredGears.forEach { (gear, numbers) ->
-//        println("Gear at (row=${gear.row}, col=${gear.col}): $numbers")
-//    }
-//
-//    filteredGears.forEach { (_, numList) ->  numList.reduce{ product, i -> product*i }}
-
-    //filteredGears.forEach { (_, num) -> println(num.first) }
-
-    println("This is version 9")
+    gears.filterValues { it.size == 2 }
+        .map { (_, value) -> value.reduce { acc, i -> acc * i } }
+        .reduce { acc, i -> acc + i }
+        .let { println("Result for part 2 is $it") }
 }
 
 fun <K, V> MutableMap<K, MutableList<V>>.addToMultimap(key: K, value: V) {
     this.computeIfAbsent(key) { mutableListOf() }.add(value)
 }
 
-data class Gear (val row : Int, val col : Int)
 fun searchNeighbourSignal(h: Int, w: Int, multiMap: Map<Int, MutableList<Int>>) : Boolean {
     return multiMap.filterKeys {
         it in h-1..h+1
@@ -90,9 +81,30 @@ fun searchNeighbourSignal(h: Int, w: Int, multiMap: Map<Int, MutableList<Int>>) 
     }
 }
 
-fun searchNeighbourGear(h: Int, w: Int, multiMap: Map<Int, MutableList<Int>>) : List<Gear> {
-    return multiMap.flatMap { (row, colList) -> colList.map { col -> Gear(row, col) }
-    }.filter {
-        gear -> gear.row in h-1..h+1 && gear.col in w-1..w+1
+fun searchGearNeighbours(gear: Gear, charArray: Array<CharArray>) : List<Int> {
+    val row = gear.row
+    val neighbours = mutableListOf<Int>()
+    var str = StringBuilder()
+    var isNear = false
+
+    for (i in row-1..row+1) {
+        for (j in 0..charArray[i].size-1) {
+            if (charArray[i][j].isDigit()) {
+                str.append(charArray[i][j])
+                isNear = isNear || isNearTheGear(i, j, gear)
+            } else {
+                if (str.isNotEmpty() && isNear) {
+                    neighbours.add(str.toString().toInt())
+                    isNear = false
+                }
+                str.clear()
+            }
+        }
+        str.clear()
     }
+    return neighbours
+}
+fun isNearTheGear(row: Int, index: Int, gear: Gear): Boolean {
+    val (gearRow, gearCol) = gear
+    return (row in gearRow-1..gearRow+1 && index in gearCol-1..gearCol+1)
 }
